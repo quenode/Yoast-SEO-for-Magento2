494976398a7e2b420a7087750b4e882fd8109fc1
branko tanovoic <tane@clarus.rs>
2023-05-31 07:38:08 +0200

    Fixed striptags

--- Model/Entity/Configuration/AbstractMetaProvider.php
+++ Model/Entity/Configuration/AbstractMetaProvider.php
@@ -70,7 +70,8 @@ abstract class AbstractMetaProvider implements MetaProviderInterface
     {
         $baseUrl = $this->urlBuilder->getBaseUrl();
         $root = $this->directoryList->getRoot() . '/';
-        $image = $image ? str_replace($baseUrl, $root, $imageUrl) : "";
+        #$image = $image ? str_replace($baseUrl, $root, $imageUrl) : "";
+        $image = str_replace($baseUrl, $root, $imageUrl);
         $meta = [];
         if (file_exists($image)) {
             $size = getimagesize($image);
